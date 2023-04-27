9244b7afd90f6eb178d622b6d5926939897a27f0
zhaojing834 <zhaojing834@jd.com>
2023-04-27 18:27:25 +0800

    core publishConfig 修改

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,7 +1,19 @@
+/*
+ * @Author: zhaojing834 zhaojing834@jd.com
+ * @Date: 2023-04-27 17:23:27
+ * @LastEditors: zhaojing834 zhaojing834@jd.com
+ * @LastEditTime: 2023-04-27 18:07:37
+ * @FilePath: /dream-cli-dev/packages/core/lib/core.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 'use strict';
 
 module.exports = core;
 
 function core() {
   return 'Hello from core';
+}
+
+function add(a, b) {
+  return a + b;
 }
